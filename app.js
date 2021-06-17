58f9a665e87e2bc2db5756edd933795794cb50a5
Lê Hoàng Chương <lehoangchuong.lk@gmail.com>
2021-06-17 09:18:36 +0700

    buoi1

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const port = 3000;
+
+// app.use(logger('dev'));
+
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// app.use((req, res, next) => {
+//   const error = app.get('env') === 'development' ? err : {};
+//   const status = err.status || 500;
+
+//   res.status(status).json({
+//     error: {
+//       message: error.message,
+//     },
+//   });
+// });
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: 'Server is Ok',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
